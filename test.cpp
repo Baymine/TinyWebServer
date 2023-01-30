391aa165844be003a51984e381216a8980395b73
Baymine <978176728@qq.com>
2023-01-30 23:10:42 +0800

    add some comments about http feature

--- test.cpp
+++ test.cpp
@@ -9,20 +9,20 @@
 #define SBUF_SIZE 128
 char sbuf[SBUF_SIZE];
 
-void MyPrintF( const char * format, ... )
-{
+void MyPrintF(const char *format, ...) {
     va_list args;
     va_start (args, format);
 
-    vsnprintf (sbuf,SBUF_SIZE,format, args);
+    vsnprintf(sbuf, SBUF_SIZE, format, args);
     printf("%d\n", va_arg(args, int));
     va_end (args);
 
-    printf("%s",sbuf);
+    printf("%s", sbuf);
 }
 
-int main()
-{
-    MyPrintF("my name is %s,my age is %d\n","bob",18,23);
+void temp() {  }
+
+int main() {
+    MyPrintF("my name is %s,my age is %d\n", "bob", 18, 23);
     return 0;
 }
